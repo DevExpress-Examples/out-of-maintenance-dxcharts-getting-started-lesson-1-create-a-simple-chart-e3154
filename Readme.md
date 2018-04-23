88592fa14059091fb6b8e3e2bb64672e08d0f9fb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DXCharts Getting Started - Lesson 1 - Create a Simple Chart
+
+
+<p>This example demonstrates how to create a simple chart, populate it with data and adjust its common settings.<br />
+</p>
+
+<br/>
+
+
---
+++ VB/WPFCharts_Lesson1/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace WPFCharts_Lesson1
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
